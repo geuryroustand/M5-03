aab250023df9d97655017aac5830fc16e4470bbd
Geury Roustand <theromantic2010@hotmail.com>
2021-08-11 17:33:21 +0200

    homeWork in Process

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-<<<<<<< HEAD
 import postRouter from "./data/index.js";
 
 const server = express();
@@ -13,18 +12,4 @@ server.use("/posts", postRouter);
 
 server.listen(port, () => {
   console.log("Server running");
-=======
-import authorRouter from "../src/data/authors.js";
-const server = express();
-
-const port = 3001;
-
-// server.use(cors());
-server.use(express.json());
-
-server.use("/authors", authorRouter);
-
-server.listen(port, () => {
-  console.log("Server listing port");
->>>>>>> 42d9fa9a7e9ef901e0926c7fe3c974820d44c4b1
 });
